2e32e743c3a4c679ffef2c2990f9f8fe89d8a6d4
tomiaJO <t.koncz@gmail.com>
2018-03-23 19:19:18 +0100

    working on reading in all subtitle files.

--- read_subtitles.R
+++ read_subtitles.R
@@ -8,52 +8,70 @@ library(ggplot2)
 
 rm(list=ls())
 
+number_of_seasons  <- 5
+list_episode_df    <- vector(mode = "list", length = number_of_seasons) 
+season_number_list <- c(1:number_of_seasons) 
+
 ##TODO: additional loop across seasons
-dirPath  <- "./../Data/Text Mining Final Project/Season1"
-
-srt_list <- list.files(dirPath)
-
-number_of_episodes <- length(srt_list)
-list_srt_df        <- vector(mode = "list", length = number_of_episodes) 
-
-episode_number_list <- c(1:number_of_episodes)
-
-for(episode_num in episode_number_list) {
-  filePath <- paste(dirPath, srt_list[[episode_num]], sep = "/")
-  srt      <- subtools::read.subtitles(file = filePath,
-                                       format = "subrip",
-                                       encoding = "latin1")
+for(season_num in season_number_list) {
   
-  srt_df <- srt$subtitles %>%
-              select(-ID) %>%
-              mutate(episode_title  = srt_list[[episode_num]],
-                     episode_number = episode_num,
-                     season_number  = 1) %>%
-              mutate(start_in_sec   = period_to_seconds(hms(Timecode.in)),
-                     end_in_sec     = period_to_seconds(hms(Timecode.out))) 
+  dirPath  <- paste("./../Data/Text Mining Final Project/Season", season_num, sep = "")
+  srt_list <- list.files(dirPath)
+
+  number_of_episodes <- length(srt_list)
+  list_srt_df        <- vector(mode = "list", length = number_of_episodes) 
+  episode_number_list <- c(1:number_of_episodes)
   
-  last_start_in_sec <- srt_df$start_in_sec %>% max()
-  last_end_in_sec   <- srt_df$end_in_sec   %>% max()
+  print(paste("Season ", season_num, ": ", number_of_episodes, sep = ""))
   
-  srt_df <- srt_df %>%
-              mutate(relative_start = start_in_sec / last_start_in_sec)
+  for(episode_num in episode_number_list) {
+    filePath <- paste(dirPath, srt_list[[episode_num]], sep = "/")
+    srt      <- subtools::read.subtitles(file = filePath,
+                                         format = "subrip",
+                                         encoding = "latin1")
+    
+    srt_df <- srt$subtitles %>%
+      select(-ID) %>%
+      mutate(episode_title  = srt_list[[episode_num]],
+             episode_number = episode_num,
+             season_number  = season_num) %>%
+      mutate(start_in_sec   = period_to_seconds(hms(Timecode.in)),
+             end_in_sec     = period_to_seconds(hms(Timecode.out))) 
+    
+    last_start_in_sec <- srt_df$start_in_sec %>% max()
+    last_end_in_sec   <- srt_df$end_in_sec   %>% max()
+    
+    srt_df <- srt_df %>%
+      mutate(relative_start = start_in_sec / last_start_in_sec)
+    
+    list_srt_df[[episode_num]] <- srt_df
+  }
   
-  list_srt_df[[episode_num]] <- srt_df
+  list_episode_df[[season_num]] <- do.call("rbind", list_srt_df)
 }
 
-srt_df <- do.call("rbind", list_srt_df)
+all_srts_df <- do.call("rbind", list_episode_df)
+
 
 
 ## check episode lengths to ensure nothing is dropped
-srt_df %>%
-  group_by(episode_number) %>%
+all_srts_df %>%
+  group_by(season_number, episode_number) %>%
   summarize(line_count  = n(),
             last_second = max(start_in_sec, na.rm = T)) %>%
-  ggplot(     aes(x = episode_number)) +
-    geom_line(aes(y = line_count))     +
-    geom_line(aes(y = last_second))
+  ggplot(    aes(x = episode_number)) +
+    geom_bar(aes(y = -1 * line_count),  stat = "identity", fill = "orange") +
+    geom_bar(aes(y = last_second),      stat = "identity", fill = "blue") +
+    facet_wrap(~season_number, scales = "free")
 
 
+all_srts_df %>% 
+  filter(season_number == 3 & episode_number == 4)
+
+
+
+
+########################
 ## basic word count viz
 data(stop_words)
 
@@ -67,9 +85,11 @@ srt_df %>%
   ggplot(aes(word, n)) +
     geom_col() +
     coord_flip() +
-    facet_wrap(~episode_number, scales = "free")
+    facet_wrap(~episode_number, scales = "free_y")
 
 
+########################
+## playing with sentiments
 sentiments_afinn <- get_sentiments("afinn") %>% rename("sentiment_afinn_score" = "score")
 sentiments_bing  <- get_sentiments("bing")  %>% rename("sentiment_bing"        = "sentiment")
 sentiments_nrc   <- get_sentiments("nrc")   %>% rename("sentiment_nrc"         = "sentiment")
@@ -80,3 +100,11 @@ words_w_sentiments <- srt_df %>%
                         left_join(sentiments_bing,  by = "word") %>%
                         left_join(sentiments_nrc,   by = "word")
   
+words_w_sentiments %>%
+  filter(!is.na(sentiment_afinn_score)) %>%
+  mutate(index = start_in_sec %/% 300) %>%
+  group_by(episode_number, index) %>%
+  summarize(sentiment = sum(sentiment_afinn_score)) %>%
+  ggplot(aes(x = index, y = sentiment)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~episode_number)
